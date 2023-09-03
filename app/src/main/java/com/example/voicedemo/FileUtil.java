582788704f56fcf7725be8dab4c532fe8a76a3e1
AandyRtb <2454003813@qq.com>
2023-09-03 17:05:48 +0800

    3

--- app/src/main/java/com/example/voicedemo/FileUtil.java
+++ app/src/main/java/com/example/voicedemo/FileUtil.java
@@ -9,48 +9,67 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
+/**
+ * 文件操作工具类
+ */
 public class FileUtil {
-    public FileUtil() {
-    }
-
+    /**
+     * 读取文件内容为二进制数组
+     *
+     * @param filePath
+     * @return
+     * @throws IOException
+     */
     public static byte[] read(String filePath) throws IOException {
+
         InputStream in = new FileInputStream(filePath);
         byte[] data = inputStream2ByteArray(in);
         in.close();
+
         return data;
     }
 
+    /**
+     * 流转二进制数组
+     *
+     * @param in
+     * @return
+     * @throws IOException
+     */
     private static byte[] inputStream2ByteArray(InputStream in) throws IOException {
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        byte[] buffer = new byte[4096];
-        //int n = false;
 
-        int n;
-        while((n = in.read(buffer)) != -1) {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        byte[] buffer = new byte[1024 * 4];
+        int n = 0;
+        while ((n = in.read(buffer)) != -1) {
             out.write(buffer, 0, n);
         }
-
         return out.toByteArray();
     }
 
+    /**
+     * 保存文件
+     *
+     * @param filePath
+     * @param fileName
+     * @param content
+     */
     public static void save(String filePath, String fileName, byte[] content) {
         try {
             File filedir = new File(filePath);
             if (!filedir.exists()) {
                 filedir.mkdirs();
             }
-
             File file = new File(filedir, fileName);
             OutputStream os = new FileOutputStream(file);
             os.write(content, 0, content.length);
             os.flush();
             os.close();
-        } catch (FileNotFoundException var6) {
-            var6.printStackTrace();
-        } catch (IOException var7) {
-            var7.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
     }
 }
 
